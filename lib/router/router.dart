39678c6bbee8168a28aea1c6dac6002beaef9b9e
LongTN <longtn@imt-soft.com>
2024-02-20 13:20:46 +0700

    docs: write document for router

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -6,28 +6,35 @@ import '../core/config/injector.dart';
 import '../module/app/app.dart';
 import '../module/auth/auth.dart';
 import '../module/home/home.dart';
+import 'go_router_refresh_stream.dart';
 
 part 'router.g.dart';
 part 'routing.dart';
 
+/// Redirect to [HomeRoute] for web
 @TypedGoRoute<InitalRoute>(path: '/')
 class InitalRoute extends GoRouteData {
   @override
   String? redirect(BuildContext context, GoRouterState state) => const HomeRoute().location;
 }
 
+/// [AuthRoute] for push, go, replace navigation
 @TypedGoRoute<AuthRoute>(path: '/auth')
 class AuthRoute extends GoRouteData {
+  /// Creates an AuthRoute
   const AuthRoute();
 
+  /// Config parent key for login screen
   static final GlobalKey<NavigatorState> $parentNavigatorKey = rootNavigatorKey;
 
   @override
   Widget build(BuildContext context, GoRouterState state) => const LoginScreen();
 }
 
+/// [HomeRoute] for push, go, replace navigation
 @TypedGoRoute<HomeRoute>(path: '/home')
 class HomeRoute extends GoRouteData {
+  /// Creates an HomeRoute
   const HomeRoute();
 
   @override
